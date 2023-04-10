dd4d5af519f287b0bf14f9957fe69d81bd95c603
himadrirathod43 <himu1467@gmail.com>
2023-04-10 14:45:37 +0530

    Add type of module

--- next.config.js
+++ next.config.js
@@ -3,12 +3,19 @@ const path = require('node:path');
 
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
+  webpack(config, options) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
-    })  
+    }),
+    config.module.rules.push({
+      test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
+      type: 'asset/resource',
+      generator: {
+          filename: 'static/chunks/[path][name].[hash][ext]'
+      },
+    }),
     config.resolve.alias = {
       ...config.resolve.alias,
       '@styles': path.resolve(__dirname, 'styles'),
