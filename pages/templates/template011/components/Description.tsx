423474511975ad0cc6090aa2fd67ccfe7b5c1827
himadrirathod43 <himu1467@gmail.com>
2023-04-11 11:18:54 +0530

    Remove build from git ignore

--- pages/templates/template011/components/Description.tsx
+++
@@ -1,9 +0,0 @@
-// import "../template01/scss/temp01.scss";
-
-type Props = {
-  descText: string;
-  className: string;
-};
-export const Description: React.FC<Props> = ({ descText, className }) => {
-  return <p className={className}>{descText}</p>;
-};
